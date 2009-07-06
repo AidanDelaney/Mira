40757f118a0d571adbb72bd5999bac9cd2325b18
Eric Kow <E.Y.Kow@brighton.ac.uk>
2009-07-06 09:35:46 +0000

    Simple Arbitrary instance for NFA. This allows us to generate random automata for use with QuickCheck.

--- NfaTypes.hs
+++ NfaTypes.hs
@@ -32,6 +32,9 @@
 
 module NfaTypes where
 
+import Control.Monad ( ap, replicateM, filterM )
+import Test.QuickCheck (Arbitrary(..), sized, vector, oneof, elements)
+
 import Sets
 
 data Move a = Move a Char a | Emove a a
@@ -40,3 +43,18 @@ data Move a = Move a Char a | Emove a a
 data Nfa a  = NFA (Set a) (Set (Move a)) a (Set a)
 	      deriving (Eq,Show)
 
+instance (Ord a, Arbitrary a) => Arbitrary (Nfa a) where
+ arbitrary = sized aut
+  where
+   aut sz =
+     do msz <- arbitrary
+        fsz <- elements [1..sz] -- seems hokey
+        --
+        states  <- vector sz
+        mvs     <- replicateM msz $ move states
+        start   <- elements states
+        final   <- take fsz `fmap` sublist states -- subset
+        return $ NFA (makeSet states) (makeSet mvs) start (makeSet final)
+   move sts = oneof [ Move  `fmap` elements sts `ap` arbitrary `ap` elements sts
+                    , Emove `fmap` elements sts `ap` elements sts ]
+   sublist = filterM (const arbitrary)
