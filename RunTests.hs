658bcb62b4dd7242a9dad6e3d341f966817e28b5
a.j.delaney <a.j.delaney@brighton.ac.uk>
2009-07-06 11:02:53 +0000

    Import correct version of QuickCheck

--- RunTests.hs
+++ RunTests.hs
@@ -3,7 +3,7 @@ where
 
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.HUnit
-import Test.Framework.Providers.QuickCheck
+import Test.Framework.Providers.QuickCheck2
 
 import Test.HUnit
 import Test.QuickCheck
