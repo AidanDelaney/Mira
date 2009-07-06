a14d9fe9ef2958404fc26eba63e0163cfe2a69ef
Eric Kow <E.Y.Kow@brighton.ac.uk>
2009-07-06 09:54:50 +0000

    Add QuickCheck to Tests.RunTests. This is very rudimentary; we'll have to upgrade to a proper test framework such as test-framework if we wanted to run more tests.

--- Tests/RunTests.hs
+++ Tests/RunTests.hs
@@ -2,7 +2,10 @@ module RunTests
 where
 
 import Test.HUnit
+import Test.QuickCheck
 import Tests.TestBuildNfa as TestBuildNfa
+import Tests.TestMinimiseDfa as TestMinimiseDfa
 
-main = 
+main = do
      runTestTT TestBuildNfa.suite
+     quickCheck TestMinimiseDfa.prop_minimiseAccept
