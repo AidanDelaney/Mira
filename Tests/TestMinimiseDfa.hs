5a00914368a5e75ba0437135214fb5a3b82281a9
a.j.delaney <a.j.delaney@brighton.ac.uk>
2009-07-18 20:44:41 +0000

    QC test to verify that DFAs are complete

--- Tests/TestMinimiseDfa.hs
+++ Tests/TestMinimiseDfa.hs
@@ -19,11 +19,15 @@ import NfaTypes
 import MinimiseDfa
 import ImplementNfa
 import NfaToDfa
+import NfaLib
+import Data.Set as Set
+import Data.List
 
 suite = testGroup "DFA minimisation"
           [ testProperty "minimised automaton accepts same strings"  prop_minimiseAccept,
 	  testProperty "a make_deterministic and an nfa accept the same strings" prop_deterministicAccept,
-	  testProperty "minimise is deterministic"  prop_minimiseDeterministic ]
+	  testProperty "minimise is deterministic"  prop_minimiseDeterministic,
+	  testProperty "minimal deterministic automata are complete"  prop_completeness ]
 
 prop_minimiseAccept :: Nfa Int -> String -> Bool
 prop_minimiseAccept nfa str = accepts ((minimise . make_deterministic) nfa) str == accepts nfa str
@@ -33,3 +37,14 @@ prop_deterministicAccept nfa str = accepts (make_deterministic nfa) str == accep
 
 prop_minimiseDeterministic :: Nfa Int -> Bool
 prop_minimiseDeterministic nfa = (minimise . minimise) nfa == minimise nfa
+
+prop_completeness :: Nfa Int -> Bool
+prop_completeness nfa = 
+		  and [(is_transition_complete (moves d) sigma q)| q <- Set.toList (states d)]
+		  where
+		  d =  make_deterministic nfa
+		  sigma = alphabet d
+
+is_transition_complete :: Set (Move a) -> [Char] -> Int -> Bool
+is_transition_complete moves alphabet state =
+		       (fromList alphabet) == (fromList [a | Move state a _ <- (toList moves)])
